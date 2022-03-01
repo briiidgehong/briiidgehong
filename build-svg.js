91ddf055d31518601ce51a32f08841fa531ea36a
briiidgehong <briiidgehong@gmail.com>
2022-03-01 21:01:04 +0900

    ENH: 소개글 수정

--- build-svg.js
+++ build-svg.js
@@ -4,8 +4,8 @@ fs.readFile("template.svg", "utf-8", (error, data) => {
   if (error) {
     return;
   }
-  
-  data = data.replace('{name}', 'uiandwe')
+
+  data = data.replace("{name}", "jiyoung");
 
   data = fs.writeFile("chat.svg", data, (err) => {
     if (err) {
